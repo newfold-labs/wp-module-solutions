afd564897058862b956ae52620ac80b70137da28
Evan Mullins <evanrm@gmail.com>
2024-11-18 16:44:30 -0500

    add tests for all scenarios that check installer attributes
    checks attributes are correct in each situation and installer functions
    pls plugin, free plugin, free install install, installed and active, installed and not active

--- tests/cypress/integration/wp-plugins-installation-check.cy.js
+++ tests/cypress/integration/wp-plugins-installation-check.cy.js
@@ -1,152 +1,424 @@
 // <reference types="Cypress" />
+import { wpLogin, wpCli, setCapability } from '../wp-module-support/utils.cy';
+
 const entitlementsFixture = require( '../fixtures/entitlements.json' );
-// path to the entitlements json to load into transient
-const entitlementsjson =
-	'vendor/newfold-labs/wp-module-solutions/tests/cypress/fixtures/entitlements.json';
-const customCommandTimeout = 20000;
-// wp cli wrapper
-const wpcli = ( cmd ) => {
-	cy.exec( `npx wp-env run cli wp ${ cmd }`, {
-		env: {
-			NODE_TLS_REJECT_UNAUTHORIZED: '1',
-		},
-	} ).then( ( result ) => {
-		for ( const [ key, value ] of Object.entries( result ) ) {
-			cy.log( `${ key }: ${ value }` );
-		}
-	} );
-};
 
-describe( 'My Plugins and Tools in Plugin App', function () {
-	after( () => {
-		wpcli( `option delete _transient_nfd_site_capabilities` );
-		wpcli( `option delete _transient_newfold_solutions` );
-		wpcli( `option delete nfd_data_token` );
-	} );
+// test each entitlement button case state
+// 1. plugin already installed and active
+// 2. plugin already installed but not active
+// 3. free plugin not installed, needs appropriate installer attributes
+// 4. premium plugin not installed, needs appropriate pls attributes
 
-	// check that it does not display when capabilities.hasSolution is false
-	it( 'My Plugins & Tools tab does not display without solution Jetpack check', () => {
-		wpcli( `option delete _transient_nfd_site_capabilities` );
-		wpcli( `option delete _transient_newfold_solutions` );
-		cy.visit( '/wp-admin/plugins.php', {
-			timeout: customCommandTimeout,
-		} );
-
-		cy.get( '#the-list' ).within( () => {
-			cy.get( 'tr[data-slug="jetpack"]' ).should( 'not.exist' );
-		} );
-		cy.get( '#the-list' ).within( () => {
-			cy.get( 'tr[data-slug="wordpress-seo"]' ).should( 'not.exist' );
-		} );
-
-		// need a cli command to set a capability before a test
-		cy.visit( '/wp-admin/plugin-install.php' );
-		// check that my plugins and tools tab displays when capabilities.hasSolution is true
-		cy.get(
-			'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
-		).should( 'not.exist' );
-		cy.get( '.plugin-install-nfd_my_plugins_and_tools' ).should(
-			'not.exist'
-		);
-	} );
-
-	it( 'My Plugins & Tools exists', () => {
-		// Set hasSolution:true in capabilities
-		wpcli(
-			`option set _transient_nfd_site_capabilities '{"hasSolution": true}' --format=json`
-		);
-		// spoof hiive connection
-		wpcli( `option set nfd_data_token 'xyc123'` );
-		// Set a long timeout for the transients
-		wpcli( `option set _transient_timeout_newfold_solutions 4102444800` );
-		wpcli(
-			`option set _transient_timeout_nfd_site_capabilities 4102444800`
-		);
-		// add test entitlements into transient
-		wpcli(
-			`option set _transient_newfold_solutions --format=json < ${ entitlementsjson }`
-		);
-
-		// reload plugin install page
-		cy.reload( true );
-		cy.visit( '/wp-admin/plugin-install.php' );
-		cy.window().then( ( win ) => {
-			cy.log(
-				`NewfoldRuntime.capabilities.hasSolution: ${ win.NewfoldRuntime.capabilities.hasSolution }`
+describe(
+	'My Plugins and Tools in Plugin App',
+	{ testIsolation: true, retries: 0 },
+	() => {
+		beforeEach( () => {
+			wpLogin();
+			cy.visit( '/wp-admin/index.php' );
+			wpCli(
+				`plugin uninstall jetpack wordpress-seo --deactivate`,
+				false
 			);
 		} );
 
-		// check that my plugins and tools tab displays when capabilities.hasSolution is true
-		cy.get(
-			'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
-		).should( 'be.visible' );
+		after( () => {
+			wpCli( `transient delete nfd_site_capabilities` );
+			// uninstall jetpack and yoast plugins
+			wpCli(
+				`plugin uninstall jetpack wordpress-seo --deactivate`,
+				false
+			);
+		} );
 
-		// check that entitlement plugins load
-		cy.visit( '/wp-admin/plugin-install.php?tab=nfd_my_plugins_and_tools' );
-		cy.reload( true );
+		// verify plugins are not yet installed
+		it( 'Yoast and Jetpack not installed', () => {
+			cy.visit( '/wp-admin/plugins.php' );
 
-		cy.get( '.plugin-install-nfd_my_plugins_and_tools' ).should(
-			'be.visible'
-		);
+			cy.get( '#the-list' ).within( () => {
+				cy.get( 'tr[data-slug="jetpack"]' ).should( 'not.exist' );
+			} );
+			cy.get( '#the-list' ).within( () => {
+				cy.get( 'tr[data-slug="wordpress-seo"]' ).should( 'not.exist' );
+			} );
+		} );
 
-		cy.get( '.nfd-solutions-availble-list' ).should( 'exist' );
+		// check a pls plugin has correct attributes on button
+		// cannot fully test pls install here because the site is not hiive registered
+		// and the pls endpoint requires a site and user with solution add-on
+		it( 'PLS plugin has proper attributes', () => {
+			// Set hasSolution:true in capabilities
+			setCapability( { hasSolution: true } );
 
-		cy.get( '.nfd-solutions-availble-list' )
-			.contains( 'h3', 'Jetpack' )
-			.scrollIntoView()
-			.should( 'be.visible' );
+			// entitlements intercept
+			cy.intercept(
+				{
+					method: 'GET',
+					url: /newfold-solutions(\/|%2F)v1(\/|%2F)entitlements/,
+				},
+				{
+					body: entitlementsFixture,
+					delay: 100,
+				}
+			).as( 'getEntitlements' );
 
-		cy.get( 'h3.nfd-solutions-availble-list-item-title', {
-			timeout: customCommandTimeout,
-		} )
-			.contains( 'Jetpack' )
-			.parent( 'div.details' )
-			.find( '.nfd-solutions-availble-list-item-button' )
-			.should( 'be.visible' )
-			.click();
-		cy.wait( 10000 );
-		cy.visit( '/wp-admin/plugins.php' );
-		cy.reload( true );
-		cy.wait( 10000 );
-		cy.get( '#the-list', {
-			timeout: customCommandTimeout,
-		} ).within( () => {
-			cy.get( 'tr[data-slug="jetpack"]', {
-				timeout: customCommandTimeout,
-			} ).should( 'exist' );
+			// load plugin install page
+			cy.visit( '/wp-admin/plugin-install.php' );
 
+			cy.window().then( ( win ) => {
+				cy.log(
+					`NewfoldRuntime.capabilities.hasSolution: ${ win.NewfoldRuntime.capabilities.hasSolution }`
+				);
+			} );
+
+			// check that my plugins and tools tab displays when capabilities.hasSolution is true
+			cy.get(
+				'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
+			)
+				.should( 'be.visible' )
+				.click();
+
+			cy.wait( '@getEntitlements' );
+
+			cy.get( '.plugin-install-nfd_my_plugins_and_tools' ).should(
+				'be.visible'
+			);
+
+			cy.get( '.nfd-solutions-availble-list' ).should( 'exist' );
+
+			// Advanced Reviews is listed
+			cy.get( '.nfd-solutions-availble-list' )
+				.contains( 'h3', 'Advanced Reviews' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+
+			// installer attributes are correct
+			cy.get( '.nfd-solutions-advanced-reviews-button' ).should(
+				'have.attr',
+				'title',
+				'Install Premium Plugin'
+			);
+			cy.get( '.nfd-solutions-advanced-reviews-button' ).should(
+				'have.attr',
+				'data-nfd-installer-plugin-name',
+				'Advanced Reviews'
+			);
+			cy.get( '.nfd-solutions-advanced-reviews-button' ).should(
+				'have.attr',
+				'data-nfd-installer-pls-slug',
+				'yith-woocommerce-advanced-reviews'
+			);
+			cy.get( '.nfd-solutions-advanced-reviews-button' ).should(
+				'have.attr',
+				'data-nfd-installer-pls-provider',
+				'yith'
+			);
+			cy.get( '.nfd-solutions-advanced-reviews-button' )
+				.should( 'have.attr', 'data-nfd-installer-plugin-url' )
+				.then( ( href ) => {
+					expect( href.includes( 'yith_ywar_panel' ) ).to.be.true;
+				} );
+			cy.get( '.nfd-solutions-advanced-reviews-button' )
+				.should( 'have.attr', 'href' )
+				.then( ( href ) => {
+					expect( href.includes( 'yith_ywar_panel' ) ).to.be.true;
+				} );
+		} );
+
+		// test free Yoast SEO plugin install functions
+		it( 'Yoast SEO plugin installs properly', () => {
+			// Set hasSolution:true in capabilities
+			setCapability( { hasSolution: true } );
+
+			// entitlements intercept
+			cy.intercept(
+				{
+					method: 'GET',
+					url: /newfold-solutions(\/|%2F)v1(\/|%2F)entitlements/,
+				},
+				{
+					body: entitlementsFixture,
+					delay: 100,
+				}
+			).as( 'getEntitlements' );
+
+			// load plugin install page
+			cy.visit( '/wp-admin/plugin-install.php' );
+
+			cy.window().then( ( win ) => {
+				cy.log(
+					`NewfoldRuntime.capabilities.hasSolution: ${ win.NewfoldRuntime.capabilities.hasSolution }`
+				);
+			} );
+
+			// check that my plugins and tools tab displays when capabilities.hasSolution is true
+			cy.get(
+				'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
+			)
+				.should( 'be.visible' )
+				.click();
+
+			cy.wait( '@getEntitlements' );
+
+			cy.get( '.plugin-install-nfd_my_plugins_and_tools' ).should(
+				'be.visible'
+			);
+
+			cy.get( '.nfd-solutions-availble-list' ).should( 'exist' );
+
+			// yoast is listed
 			cy.get( '.nfd-solutions-availble-list' )
 				.contains( 'h3', 'Yoast SEO' )
 				.scrollIntoView()
 				.should( 'be.visible' );
 
-			cy.get( 'h3.nfd-solutions-availble-list-item-title', {
-				timeout: customCommandTimeout,
-			} )
-				.contains( 'Yoast SEO' )
-				.parent( 'div.details' )
-				.find( '.nfd-solutions-availble-list-item-button' )
+			cy.get( '.nfd-solutions-yoast-seo-button' ).should(
+				'have.attr',
+				'data-nfd-installer-plugin-name',
+				'Yoast SEO'
+			);
+			cy.get( '.nfd-solutions-yoast-seo-button' ).should(
+				'have.attr',
+				'data-nfd-installer-download-url',
+				'https://downloads.wordpress.org/plugin/wordpress-seo.latest-stable.zip'
+			);
+			cy.get( '.nfd-solutions-yoast-seo-button' )
+				.should( 'have.attr', 'data-nfd-installer-plugin-url' )
+				.then( ( href ) => {
+					expect( href.includes( 'wpseo_dashboard' ) ).to.be.true;
+				} );
+			cy.get( '.nfd-solutions-yoast-seo-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-pls-slug'
+			);
+			cy.get( '.nfd-solutions-yoast-seo-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-pls-provider'
+			);
+
+			// install jetpack
+			cy.get( '.nfd-solutions-yoast-seo-button' ).click();
+
+			// verify install modal opens
+			cy.get( '.nfd-installer-modal__content' ).should( 'be.visible' );
+			cy.get( '.nfd-installer-modal__content-subheading' ).contains(
+				'Yoast'
+			);
+			// verify modal closes
+			cy.get( '.nfd-installer-modal__content', {
+				timeout: 20000,
+			} ).should( 'not.exist' );
+			cy.wait( 2000 );
+
+			// verify redirect occurred
+			cy.url().should( 'contain', 'wpseo_dashboard' );
+
+			// reload plugin list
+			cy.visit( '/wp-admin/plugins.php' );
+
+			// verify wordpress-seo is installed
+			cy.get( 'tr[data-slug="wordpress-seo"]' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+
+			// verify wordpress-seo is active
+			cy.get( 'a#deactivate-wordpress-seo' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+		} );
+
+		// test free Jetpack plugin install functions
+		it( 'Jetpack plugin installs properly', () => {
+			// Set hasSolution:true in capabilities
+			setCapability( { hasSolution: true } );
+
+			// entitlements intercept
+			cy.intercept(
+				{
+					method: 'GET',
+					url: /newfold-solutions(\/|%2F)v1(\/|%2F)entitlements/,
+				},
+				{
+					body: entitlementsFixture,
+					delay: 100,
+				}
+			).as( 'getEntitlements' );
+
+			// load plugin install page
+			cy.visit( '/wp-admin/plugin-install.php' );
+
+			cy.window().then( ( win ) => {
+				cy.log(
+					`NewfoldRuntime.capabilities.hasSolution: ${ win.NewfoldRuntime.capabilities.hasSolution }`
+				);
+			} );
+
+			// check that my plugins and tools tab displays when capabilities.hasSolution is true
+			cy.get(
+				'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
+			)
 				.should( 'be.visible' )
 				.click();
-			cy.wait( 10000 );
-			cy.visit( '/wp-admin/plugins.php', {
-				timeout: customCommandTimeout,
-			} );
-			cy.reload( true );
-			cy.wait( 10000 );
-			cy.get( '#the-list', {
-				timeout: customCommandTimeout,
-			} ).within( () => {
-				cy.get( 'tr[data-slug="wordpress-seo"]', {
-					timeout: customCommandTimeout,
-				} ).should( 'exist' );
-			} );
-		} );
-	} );
 
-	// test each entitlement button case state
-	// 1. plugin already installed and active
-	// 2. plugin already installed but not active
-	// 3. free plugin not installed, needs appropriate installer attributes
-	// 4. premium plugin not installed, needs appropriate pls attributes
-} );
+			cy.wait( '@getEntitlements' );
+
+			cy.get( '.plugin-install-nfd_my_plugins_and_tools' ).should(
+				'be.visible'
+			);
+
+			cy.get( '.nfd-solutions-availble-list' ).should( 'exist' );
+
+			// jetpack is listed
+			cy.get( '.nfd-solutions-availble-list' )
+				.contains( 'h3', 'Jetpack' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+
+			// jetpack button has proper installer attributes
+			/*
+		<button
+			title="Install Plugin"
+			class="nfd-solutions-availble-list-item-button"
+			data-nfd-installer-pls-provider="null"
+			data-nfd-installer-plugin-activate="true"
+			data-nfd-installer-plugin-name="Jetpack"
+			data-nfd-installer-download-url="https://downloads.wordpress.org/plugin/jetpack.latest-stable.zip"
+			data-nfd-installer-plugin-url="/wp-admin/admin.php?page=my-jetpack"
+		>
+			Manage
+		</button> 
+		 */
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'title',
+				'Install Plugin'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' )
+				.should( 'have.attr', 'href' )
+				.then( ( href ) => {
+					expect( href.includes( 'my-jetpack' ) ).to.be.true;
+				} );
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'data-nfd-installer-plugin-name',
+				'Jetpack'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'data-nfd-installer-download-url',
+				'https://downloads.wordpress.org/plugin/jetpack.latest-stable.zip'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' )
+				.should( 'have.attr', 'data-nfd-installer-plugin-url' )
+				.then( ( href ) => {
+					expect( href.includes( 'my-jetpack' ) ).to.be.true;
+				} );
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-pls-slug'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-pls-provider'
+			);
+
+			// install jetpack
+			cy.get( '.nfd-solutions-jetpack-button' ).click();
+
+			// verify install modal opens
+			cy.get( '.nfd-installer-modal__content' ).should( 'be.visible' );
+			cy.get( '.nfd-installer-modal__content-subheading' ).contains(
+				'Jetpack'
+			);
+			// verify modal closes
+			cy.get( '.nfd-installer-modal__content', {
+				timeout: 20000,
+			} ).should( 'not.exist' );
+			cy.wait( 2000 );
+
+			// verify redirect occurred
+			cy.url().should( 'contain', 'jetpack' );
+
+			// go to plugin list page
+			cy.visit( '/wp-admin/plugins.php' );
+
+			// verify jetpack is installed
+			cy.get( 'tr[data-slug="jetpack"]' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+
+			// verify jetpack is active
+			cy.get( 'a#deactivate-jetpack' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+
+			// check button attributes for active plugin
+			cy.get(
+				'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
+			)
+				.should( 'be.visible' )
+				.click();
+			// jetpack is listed
+			cy.get( '.nfd-solutions-availble-list' )
+				.contains( 'h3', 'Jetpack' )
+				.scrollIntoView()
+				.should( 'be.visible' );
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-plugin-name'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-plugin-url'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'title',
+				'Manage Plugin'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' )
+				.should( 'have.attr', 'href' )
+				.then( ( href ) => {
+					expect( href.includes( 'my-jetpack' ) ).to.be.true;
+				} );
+			// deactivate plugin
+			cy.visit( '/wp-admin/plugins.php' );
+			cy.get( 'a#deactivate-jetpack' ).click();
+			// attributes match expectations for installed and inactive plugin
+			cy.get(
+				'#adminmenu a[href="plugin-install.php?tab=nfd_my_plugins_and_tools"]'
+			)
+				.should( 'be.visible' )
+				.click();
+
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'title',
+				'Activate Plugin'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'data-nfd-installer-plugin-name',
+				'Jetpack'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'have.attr',
+				'data-nfd-installer-download-url',
+				'https://downloads.wordpress.org/plugin/jetpack.latest-stable.zip'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' )
+				.should( 'have.attr', 'data-nfd-installer-plugin-url' )
+				.then( ( href ) => {
+					expect( href.includes( 'my-jetpack' ) ).to.be.true;
+				} );
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-pls-slug'
+			);
+			cy.get( '.nfd-solutions-jetpack-button' ).should(
+				'not.have.attr',
+				'data-nfd-installer-pls-provider'
+			);
+		} );
+	}
+);
