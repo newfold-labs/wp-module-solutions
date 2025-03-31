dc505f68a862e8b074dd397b6297a98b96f6882c
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-03-31 15:53:35 +0100

    update premium button hover

--- build/addnew/bundle.asset.php
+++ build/addnew/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8d2f3829e028ca03f082');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2891c408d185a156aebd');
