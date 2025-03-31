8e5f4ddcefcd0ffa567a9d275ce26f0d262433a1
Evan Mullins <evanrm@gmail.com>
2025-03-31 13:32:33 -0400

    add solutions learn more link with utm params

--- src/solutions-page/components/header/Header.js
+++ src/solutions-page/components/header/Header.js
@@ -3,10 +3,12 @@ import { Title } from '@newfold/ui-component-library';
 import { __ } from '@wordpress/i18n';
 
 export const Header = () => {
-	const currentSolution = NewfoldSolutions?.solutions.find( solution => solution?.sku === NewfoldSolutions?.solution );
+	const currentSolution = NewfoldSolutions?.solutions.find(
+		( solution ) => solution?.sku === NewfoldSolutions?.solution
+	);
 	return (
 		<div className="nfd-solutions-app-header nfd-flex nfd-flex-col nfd-gap-y-4 nfd-mb-8">
-			<BrandLogo width={ '140px' } className="nfd-mb-8"/>
+			<BrandLogo width={ '140px' } className="nfd-mb-8" />
 			<Title>
 				{ __(
 					'Premium tools available in your {plan_name} solution',
@@ -18,8 +20,12 @@ export const Header = () => {
 					'Discover the complete list of advanced features provided by your hosting provider Bluehost, designed to deliver unmatched value and elevate your online experience.',
 					'wp-module-solutions'
 				) }
-				<br/>
-				<a href={ '#' }>
+				<br />
+				<a
+					href="https://www.bluehost.com/my-account/account-center?utm_source=wp-admin%2Fadmin.php%3Fpage%3Dsolutions&utm_medium=bluehost_plugin"
+					target="_blank"
+					rel="noreferrer"
+				>
 					{ __(
 						'Learn more about our Solutions',
 						'wp-module-solutions'
