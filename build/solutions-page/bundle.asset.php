31bce0f295fa136771f35fd57a538ee809ded160
Evan Mullins <evanrm@gmail.com>
2025-04-01 15:36:51 -0400

    update installer attributes and app design tweaks

--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b71ab6fbb10352bbb27a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '1491bb0aa77449c9f42c');
