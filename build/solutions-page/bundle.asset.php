4f9e99b4595d5c2fc627991fe5e2d71599d8c887
Evan Mullins <evanrm@gmail.com>
2025-04-01 12:05:35 -0400

    update build

--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '4a30219f82e23517e000');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b71ab6fbb10352bbb27a');
