47010ab9db72d9b64847db9d7dc0fdc45b13c7a5
Giuseppe Arcifa <giuseppe_arcifa@libero.it>
2025-04-01 14:52:35 +0200

    Commented development code to retireve the json ad hiive response

--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0a40d4704dd23477d869');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '727225014f021748715e');
