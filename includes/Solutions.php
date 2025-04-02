83b1011f967f586af3f1029fd77254d5859efe37
Evan Mullins <evanrm@gmail.com>
2025-04-02 17:23:24 -0400

    only enqueue assets for each app when needed

--- includes/Solutions.php
+++ includes/Solutions.php
@@ -122,97 +122,64 @@ class Solutions {
 	 * Enqueue assets and set locals.
 	 */
 	public static function solutions_page_assets() {
-		if ( isset( $screen->id ) && false !== strpos( $screen->id, 'solutions' ) ) {
+		$asset_file = NFD_SOLUTIONS_DIR . '/build/solutions-page/bundle.asset.php';
+		if ( is_readable( $asset_file ) ) {
+			$asset = include_once $asset_file;
+		} else {
 			return;
 		}
 
-		$assets_info = include NFD_SOLUTIONS_DIR . '/build/solutions-page/bundle.asset.php';
-
-		\wp_enqueue_script(
+		wp_register_script(
 			'solutions-page',
 			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/build/solutions-page/bundle.js',
 			array_merge(
-				$assets_info['dependencies'],
+				$asset['dependencies'],
 				array( 'nfd-installer' ),
 			),
-			$assets_info['version'],
+			$asset['version'],
 			true
 		);
 
-		\wp_enqueue_style(
+		wp_register_style(
 			'solutions-page-style',
 			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/build/solutions-page/solutions-page.css',
 			array( 'nfd-installer' ),
-			$assets_info['version']
+			$asset['version']
 		);
 
-		\wp_enqueue_style(
+		wp_register_style(
 			'solutions-page-style-common',
 			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/build/solutions-page/style-solutions-page.css',
-			$assets_info['version']
+			$asset['version']
 		);
 
-		$solutions_data = json_decode( wp_json_encode( self::$entitlements_api->get_items()->data ), true );
+		// Only enqueue on solutions page
+		$screen = get_current_screen();
+		if ( isset( $screen->id ) && false !== strpos( $screen->id, 'solution' ) ) {
+			wp_enqueue_script( 'solutions-page' );
+			wp_enqueue_style( 'solutions-page-style' );
+			wp_enqueue_style( 'solutions-page-style-common' );
+			
+			$solutions_data = json_decode( wp_json_encode( self::$entitlements_api->get_items()->data ), true );
 
-		if ( array_key_exists( 'entitlements', $solutions_data ) ) {
-			$solutions_data['entitlements'] = array_map(
-				function ( $entitlement ) {
-					$entitlement['isActive'] = is_plugin_active( $entitlement['basename'] );
-					return $entitlement;
-				},
-				$solutions_data['entitlements']
+			if ( array_key_exists( 'entitlements', $solutions_data ) ) {
+				$solutions_data['entitlements'] = array_map(
+					function ( $entitlement ) {
+						$entitlement['isActive'] = is_plugin_active( $entitlement['basename'] );
+						return $entitlement;
+					},
+					$solutions_data['entitlements']
+				);
+			}
+
+			\wp_localize_script(
+				'solutions-page',
+				'NewfoldSolutions',
+				array_merge(
+					$solutions_data,
+				)
 			);
 		}
-
-		\wp_localize_script(
-			'solutions-page',
-			'NewfoldSolutions',
-			array_merge(
-				$solutions_data,
-			)
-		);
-	}
-
-	/**
-	 * Enqueue assets and set locals.
-	 */
-	public static function my_plugins_and_tools_tab_enqueue_assets() {
-		if ( false === ( isset( $_GET['tab'] ) && 'nfd_my_plugins_and_tools' === $_GET['tab'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			return;
-		}
-
-		\wp_enqueue_style(
-			'nfd_myplugin_solutions_css',
-			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/includes/css/myPluginsTools.css',
-			// Note the dependency on the installer styles to ensure the installer module styles are loaded.
-			array( 'nfd-installer' ),
-			'1.1.0'
-		);
-
-		\wp_enqueue_script(
-			'nfd_myplugin_solutions_js',
-			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/includes/js/myPluginsTools.js',
-			// Note the dependency on the installer script to ensure the installer module scripts is loaded.
-			// If the installer module is not also loaded via brand plugin, this solutions script will not load.
-			array( 'nfd-installer' ),
-			'1.1.0',
-			true
-		);
-
-		\wp_localize_script(
-			'nfd_myplugin_solutions_js',
-			'nfdPluginDetails',
-			array(
-				'installed' => get_plugins(),
-				'active'    => get_option( 'active_plugins' ),
-			)
-		);
-
-		\wp_set_script_translations(
-			'nfd_myplugin_solutions_js',
-			'wp-module-solutions',
-			NFD_SOLUTIONS_DIR . '/languages'
-		);
 	}
 
 	/**
@@ -262,73 +229,82 @@ class Solutions {
 	 * @param string $hook The current admin page.
 	 */
 	public function plugins_solutions_tab_assets( $hook ) {
-		if ( 'plugin-install.php' !== $hook ) {
+
+		$asset_file = NFD_SOLUTIONS_DIR . '/build/addnew/bundle.asset.php';
+		if ( is_readable( $asset_file ) ) {
+			$asset = include_once $asset_file;
+		} else {
 			return;
 		}
-
-		$assets_info = include NFD_SOLUTIONS_DIR . '/build/addnew/bundle.asset.php';
-
-		\wp_enqueue_script(
+		\wp_register_script(
 			'solutions-add-new-tools',
 			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/build/addnew/bundle.js',
 			array_merge(
-				$assets_info['dependencies'],
+				$asset['dependencies'],
 				array( 'nfd-installer' ),
 			),
-			$assets_info['version'],
+			$asset['version'],
 			true
 		);
 
-		\wp_enqueue_style(
+		\wp_register_style(
 			'solutions-add-new-style',
 			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/build/addnew/style-addnew.css',
 			array( 'nfd-installer' ),
-			$assets_info['version']
+			$asset['version']
 		);
-		\wp_enqueue_style(
+		\wp_register_style(
 			'solutions-add-new',
 			NFD_SOLUTIONS_PLUGIN_URL . 'vendor/newfold-labs/wp-module-solutions/build/addnew/addnew.css',
 			array( 'nfd-installer' ),
-			$assets_info['version']
+			$asset['version']
 		);
 
-		$solutions_data = json_decode( wp_json_encode( self::$entitlements_api->get_items()->data ), true );
+		// Only enqueue on solutions page
+		$screen = get_current_screen();
+		if ( 'plugin-install.php' === $hook ) {
+			wp_enqueue_script( 'solutions-add-new-tools' );
+			wp_enqueue_style( 'solutions-add-new-style' );
+			wp_enqueue_style( 'solutions-add-new' );
 
-		if ( array_key_exists( 'entitlements', $solutions_data ) ) {
-			$solutions_data['entitlements'] = array_map(
-				function ( $entitlement ) {
-					$entitlement['isActive'] = is_plugin_active( $entitlement['basename'] );
-					return $entitlement;
-				},
-				$solutions_data['entitlements']
-			);
-		}
+			$solutions_data = json_decode( wp_json_encode( self::$entitlements_api->get_items()->data ), true );
 
-		\wp_localize_script(
-			'solutions-add-new-tools',
-			'NewfoldSolutions',
-			array_merge(
-				$solutions_data,
-				array(
-					'siteUrl' => get_site_url(),
+			if ( array_key_exists( 'entitlements', $solutions_data ) ) {
+				$solutions_data['entitlements'] = array_map(
+					function ( $entitlement ) {
+						$entitlement['isActive'] = is_plugin_active( $entitlement['basename'] );
+						return $entitlement;
+					},
+					$solutions_data['entitlements']
+				);
+			}
+
+			\wp_localize_script(
+				'solutions-add-new-tools',
+				'NewfoldSolutions',
+				array_merge(
+					$solutions_data,
+					array(
+						'siteUrl' => get_site_url(),
+					)
 				)
-			)
-		);
+			);
 
-		$script = "
-        document.addEventListener('DOMContentLoaded', function() {
-            let icon = `<svg id='ndf-tools-plugin-bluehost-brand' width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path fill-rule='evenodd' clip-rule='evenodd'
-                      d='M16 4.46067V0H11.5302V4.46067H16ZM16 5.76933V10.2307H11.5302V5.76933H16ZM4.46778 16V11.5387H0V16H4.46778ZM10.2339 11.5387V16H5.76409V11.5387H10.2339ZM16 11.5387V16H11.5302V11.5387H16ZM10.2339 10.2307V5.76933H5.76409V10.2307H10.2339ZM4.46778 5.76933V10.2307H0V5.76933H4.46778ZM10.2305 0V4.46067H5.76409V0H10.2305ZM4.46778 4.46067V0H0V4.46067H4.46778Z'
-                      fill='#196BDE'/>
-            </svg>`;
-            const filterPremiumLink = document.querySelector('.plugin-install-nfd_solutions > a');
-            if (filterPremiumLink) {
-                filterPremiumLink.innerHTML = icon + filterPremiumLink.innerHTML;
-            }
-        });
-    ";
+			$script = "
+			document.addEventListener('DOMContentLoaded', function() {
+				let icon = `<svg id='ndf-tools-plugin-bluehost-brand' width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+					<path fill-rule='evenodd' clip-rule='evenodd'
+						d='M16 4.46067V0H11.5302V4.46067H16ZM16 5.76933V10.2307H11.5302V5.76933H16ZM4.46778 16V11.5387H0V16H4.46778ZM10.2339 11.5387V16H5.76409V11.5387H10.2339ZM16 11.5387V16H11.5302V11.5387H16ZM10.2339 10.2307V5.76933H5.76409V10.2307H10.2339ZM4.46778 5.76933V10.2307H0V5.76933H4.46778ZM10.2305 0V4.46067H5.76409V0H10.2305ZM4.46778 4.46067V0H0V4.46067H4.46778Z'
+						fill='#196BDE'/>
+				</svg>`;
+				const filterPremiumLink = document.querySelector('.plugin-install-nfd_solutions > a');
+				if (filterPremiumLink) {
+					filterPremiumLink.innerHTML = icon + filterPremiumLink.innerHTML;
+				}
+			});
+		";
 
-		wp_add_inline_script( 'solutions-add-new-tools', $script );
+			wp_add_inline_script( 'solutions-add-new-tools', $script );
+		}
 	}
 }
