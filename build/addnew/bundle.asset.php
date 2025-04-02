c84d663bbbeeea080cfe42e31d9fc73b9c961f6f
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-04-02 12:35:04 +0100

    Tweak load a custom CSS class and apply some style changes

--- build/addnew/bundle.asset.php
+++ build/addnew/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b05b247c8ea4f484bc12');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8d8949d00bb468f1036c');
