42c8390b916efbc8e3ad9bd450817915b60d2533
Evan Mullins <evanrm@gmail.com>
2025-04-03 13:20:43 -0400

    code cleanup and update build files

--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '30e70851b89ce49a9070');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '11e40cd47f82c963fe77');
