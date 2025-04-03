8d17c7c21d12eacf0be11d220e85ec92deef4b38
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-04-03 16:35:01 +0100

    Add test for different solution

--- tests/cypress/integration/my-solutions-app.cy.js
+++ tests/cypress/integration/my-solutions-app.cy.js
@@ -12,10 +12,9 @@ describe( 'My Solution under Plugins > Add new', { testIsolation: true }, () => 
 		wpCli( `transient delete nfd_site_capabilities` );
 	} );
 
-    // since the test site is not connected to hiive, it will not have a solution
     it( 'The Bluehost brand logo is showed on Bluehost solutions tab', () => {
 
-        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions' );
+        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions&solution=none');
 
         cy.window().then( ( win ) => {
 
@@ -32,7 +31,7 @@ describe( 'My Solution under Plugins > Add new', { testIsolation: true }, () => 
 
 	it( 'My Solution page is rendered with no solution', () => {
 
-        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions' );
+        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions&solution=none' );
 
         cy.window().then( ( win ) => {
 
@@ -46,4 +45,66 @@ describe( 'My Solution under Plugins > Add new', { testIsolation: true }, () => 
 			.scrollIntoView()
 			.should( 'be.visible' );
 	} );
+    it( 'My Solution page is rendered with Commerce solution', () => {
+
+        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions&solution=commerce' );
+
+        cy.window().then( ( win ) => {
+
+            cy.log(
+                `NewFold solution: ${ win.NewfoldSolutions.solution }`
+            )
+        } );
+
+        cy.get( '#nfd-add-new-app' )
+            .contains( 'h1', 'Premium tools available in your Commerce' )
+            .scrollIntoView()
+            .should( 'be.visible' );
+
+        
+
+    } );
+    it( 'My Solution page is rendered with Creator solution', () => {
+
+        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions&solution=creator' );
+
+        cy.window().then( ( win ) => {
+
+            cy.log(
+                `NewFold solution: ${ win.NewfoldSolutions.solution }`
+            )
+        } );
+
+        cy.get( '#nfd-add-new-app' )
+            .contains( 'h1', 'Premium tools available in your Creator' )
+            .scrollIntoView()
+            .should( 'be.visible' );
+
+        cy.get( '.nfd-solutions-upgrade-banner' )
+            .contains( 'h2', 'Upgrade' )
+            .scrollIntoView()
+            .should( 'be.visible' );
+
+    } );
+    it( 'My Solution page is rendered with Service solution', () => {
+
+        cy.visit( '/wp-admin/plugin-install.php?tab=nfd_solutions&solution=service' );
+
+        cy.window().then( ( win ) => {
+
+            cy.log(
+                `NewFold solution: ${ win.NewfoldSolutions.solution }`
+            )
+        } );
+
+        cy.get( '#nfd-add-new-app' )
+            .contains( 'h1', 'Premium tools available in your Service' )
+            .scrollIntoView()
+            .should( 'be.visible' );
+
+        cy.get( '.nfd-solutions-upgrade-banner' )
+            .contains( 'h2', 'Upgrade' )
+            .scrollIntoView()
+            .should( 'be.visible' );
+    } );
 } );
