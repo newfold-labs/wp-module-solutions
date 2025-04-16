a4f24887b66086a9ffa4cce5e70db3bf0c72c679
Evan Mullins <evanrm@gmail.com>
2025-04-16 11:19:10 -0400

    use production build files

--- build/addnew/bundle.asset.php
+++ build/addnew/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3b0a2eb2878e1b895eaf');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3098ac718b053eef1a9d');
--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6dcbfa3aa1d4bfa04713');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f797ee9d36dfa3763509');
