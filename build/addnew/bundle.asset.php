2c729a119de3e93288f219a8654ca0f466fb5855
Evan Mullins <evanrm@gmail.com>
2025-04-24 13:32:09 -0400

    update build and language files

--- build/addnew/bundle.asset.php
+++ build/addnew/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '205a07217e7a49e4b4e1');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b606bec71002be1703a9');
--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '86eb7515828131785930');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '17a1b9036d8e278838bc');
