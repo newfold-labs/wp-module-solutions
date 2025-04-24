ff3e4643cce82c8db9b95d65fd1b34e7cc477071
Evan Mullins <evanrm@gmail.com>
2025-04-24 14:33:12 -0400

    one more copy edit

--- build/addnew/bundle.asset.php
+++ build/addnew/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b606bec71002be1703a9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a1f3c0fcaebd8ce94825');
--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '17a1b9036d8e278838bc');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0b6ad6e8611be1bd4213');
