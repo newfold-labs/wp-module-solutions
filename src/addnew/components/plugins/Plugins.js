ecbeb67488b95f8ae7d025429603e6c9b05923b2
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-05-09 10:18:29 +0100

    Include search plugin on solutions tab

--- src/addnew/components/plugins/Plugins.js
+++ src/addnew/components/plugins/Plugins.js
@@ -6,36 +6,38 @@ import { useState, useEffect } from 'react';
 
 export const Plugins = ( { plan } ) => {
 
-    const [hideSolution, setHideSolution] = useState(false);
-    const [prevRenderSolution, setPrevRenderSolution] = useState(false);
-    const searchPluginsSection = document.querySelector('.ndf-plugin-filter-section');
+     const [hideSolution, setHideSolution] = useState(false);
+     const pluginSection = document.querySelector('.nfd-my-solutions-app-container');
+     const brandTab = document.querySelector('.plugin-install-nfd_solutions a');
 
     useEffect(() => {
         const input = document.querySelector('#search-plugins');
         if (!input) return;
-
         const handler = () => {
-            setHideSolution(input.value.trim().length > 0);
-            setPrevRenderSolution(true);
+            setHideSolution(input.value.trim().length > 1);
         };
 
         input.addEventListener('input', handler);
-
-        setHideSolution(input.value.trim().length > 0);
 
         return () => {
             input.removeEventListener('input', handler);
         };
     }, []);
 
-    if ( hideSolution || prevRenderSolution ) {
-        searchPluginsSection.style.display = "block";
-        return;
-    } else {
-        searchPluginsSection.style.display="none";
+    //Logic to manage to hide/show the solution section
+    if ( pluginSection ) {
+        if ( hideSolution ) {
+            pluginSection.style.display = "none";
+            // Remove the label so that the form doesn't drop below the tabs on small screens.
+            document.querySelector('.search-plugins label').style.display="none";
+        } else {
+            pluginSection.style.display = (brandTab && brandTab.classList.contains('current'))
+                ? "block"
+                : "none";
+        }
     }
 
-	return (
+    return (
 		<>
 			{ !! plan && 'WP_SOLUTION_CREATOR' === plan.sku && (
 				<Creator plan={ plan } />
