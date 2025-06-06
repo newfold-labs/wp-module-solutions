f12f13a7a41a848aced67ee9e135c7281e70670a
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-06-06 16:06:38 +0100

    Update interface for no solution plan

--- build/solutions-page/bundle.asset.php
+++ build/solutions-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a6cdb1cbfe745c866e2a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '08300cb98adc83eed3f3');
