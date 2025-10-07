2ea059542419b5528dcbe512323a630b3b84e5d1
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-10-07 13:30:42 +0100

    Fix: preserve hash in URL

--- build/solutions-page-component/index.asset.php
+++ build/solutions-page-component/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'abea4c40ea333f116ab0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd6d4fec78a228d3bb63f');
