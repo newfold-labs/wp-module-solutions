6aab2d09251ee40041a0de1678985d7932fe0db6
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-10-13 16:46:46 +0100

    Tweak allow search by tags

--- build/solutions-page-component/index.asset.php
+++ build/solutions-page-component/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '564b2f2ca85cac97f577');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a1891989f1732610a47b');
