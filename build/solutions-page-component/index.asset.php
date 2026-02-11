a080d7d4f03e2d435b103ad16aef0ae64e8ee8d9
Evan Mullins <evanrm@gmail.com>
2026-02-11 16:15:50 -0500

    update build files

--- build/solutions-page-component/index.asset.php
+++ build/solutions-page-component/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2d6ef5c64e1f1f275ad9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ac091f05c0dfac7eb630');
